a063c188d5e77a14077499a9be967216e3c1fac1
Klim Radostev <klim.radostav@axiado.com>
2022-07-07 09:08:41 -0700

    nasty bits operation

---
+++ ex00/decrypt.c
@@ -0,0 +1,28 @@
+#include "header.h"
+#include <stdint.h>
+#include <stddef.h>
+
+int toInt(char *bits)
+{
+
+}
+
+char *getSum(char *a, char *b)
+{
+    char ret[6] = {0};
+    char holder = 0;
+
+    for (size_t i = 0; i < 6; i++)
+    {
+        // if all 3 are 1
+        if (a[i] == '1' && b[i] == '1')
+    }
+    return (ret);
+}
+
+
+
+int main()
+{
+    return (0);
+}

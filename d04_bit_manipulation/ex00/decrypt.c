32eb15d5fefc8d4dd23674de6104a969a0a1ed89
KlimRadostev <klim_radostev@hotmail.com>
2024-10-21 00:06:49 -0700

    42-INTERVIEW : how to avoid for loop

--- d04_bit_manipulation/ex00/decrypt.c
+++ d04_bit_manipulation/ex00/decrypt.c
@@ -4,17 +4,41 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+typedef enum {
+    NONE = 0,
+    ADD,
+} operator_t;
+
+uint8_t get_op(char c)
+{
+    switch(c) {
+        case ('+'):
+            return ADD;
+    }
+    return NONE;
+}
+
+uint32_t to_int(char *bits, size_t sz)
+{
+    uint32_t ret = 0;
+    for (size_t i = 0; i < sz; i++) {
+        ret <<= 1;
+        if (bits[i] == '1') { ret |= 1; }
+    }
+    return ret;
+}
+
+char *get_sum(char *a, char *b);
+
 int main(int ac, char *argv[])
 {
     /* import libft helper library */
-
-    FT_STATUS status = FT_STATUS_SUCCESS;
 
     if (ac != 4) {
         return -1;
     }
 
-    uint8_t op = get_op(argv[2], );
+    uint8_t op = get_op(argv[2][0]);
     FT_STATUS = 
 
     return 0;

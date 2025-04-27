7c5f006800b79cb18610e3aa5321d5ded87297db
Klim Radostev <klimradostev@Klims-MacBook-Pro.local>
2025-04-26 17:11:07 -0700

    42-INTERVIEW : ex00 completed

--- d04_bit_manipulation/ex00/decrypt.c
+++ d04_bit_manipulation/ex00/decrypt.c
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#define BIT(x) (1 << (x))
+
 typedef enum {
     NONE = 0,
     ADD,
@@ -11,6 +13,7 @@ typedef enum {
 
 uint8_t get_op(char c)
 {
+
     switch(c) {
         case ('+'):
             return ADD;
@@ -20,73 +23,88 @@ uint8_t get_op(char c)
 
 uint32_t bit_sum(uint32_t x, uint32_t y)
 {
-    /*
-    00001111
-    +
-    00000001
-    =
-    00010000
-
-    01010101 : 0x55
-    +
-    00111111 : 0x3F
-    =
-    10010100 : 0x94
-    */
     // rules
-    // 1 + 0 = 1
-    // 1 + 1 = 0 + carry_over
-    // 1 + 1 + carry_over = 1 + carry_over
+    // 1 + 1 + true = 1 true
+    // 1 + 1 + false = 0 true
+    // 1 + 0 + true = 0 true
+    // 0 + 0 + true = 1 false
+    // 1 + 0 + false = 1 false
+    // 0 + 0 + false = 0
 
     bool carry = false;
-    for (size_t i = 0; i < 6; i ++) {
-        int a, b, c;
-        a = b = c = 0;
+    uint32_t i = 1;
+    uint32_t r = 0;
 
-        if (carry == true)
-            a = 1;
-        if (a[i] == '1')
-            b = 1;
-        if (b[i] == '1')
-            c = 1;
-        int total = a + b + c;
-        if (total == 3) {
+    do {
+        if ((x & i) != 0 && (y & i) != 0 && carry == true) {
+            r |= i;
             carry = true;
-            a[i] = 1;
-        } else if (total == 2)
-    }
+        } else if ((x & i) != 0 && (y & i) != 0 && carry == false) {
+            carry = true;
+        } else if (((x & i) != 0 || (y & i) != 0) && carry == true) {
+            carry = true;
+        } else if ((x & i) == 0 && (y & i) == 0 && carry == true) {
+            r |= i;
+            carry = false;
+        } else if (((x & i) != 0 || (y & i) != 0) && carry == false) {
+            r |= i;
+            carry = false;
+        } else if ((x & i) == 0 && (y & i) == 0 && carry == false) {
+            carry = false;
+        }
+        i <<= 1;
+    } while (i < 0x80000000);
 
-    uint32_t res1 = x | y;
-    uint32_t res2 = x & y;
-    uint32_t res = 
-}
-
-uint32_t bit_increment(uint32_t x)
-{
-    x + 1;
-
-    00001000
-    00001001
-    ...
-    00001111
-    00010000
+    return r;
 }
 
 uint32_t to_int(char *bits, size_t sz)
 {
     uint32_t ret = 0;
+    uint32_t ret_i = BIT(sz - 1);
     size_t i = 0;
     while (i < sz) {
-        ret <<= 1;
-        if (bits[i] == '1') { ret |= 1; }
-        i = bit_increment(i);
+        if (bits[i] == '1') { ret |= ret_i; }
+        i = bit_sum(i, 1);
+        ret_i >>= 1;
     }
     return ret;
 }
 
 char *get_sum(char *a, char *b)
 {
+    // rules
+    // 1 + 1 + true = 1 true
+    // 1 + 1 + false = 0 true
+    // 1 + 0 + true = 0 true
+    // 0 + 0 + true = 1 false
+    // 1 + 0 + false = 1 false
+    // 0 + 0 + false = 0 false
 
+    bool carry = false;
+    for (int i = 5; i >= 0; i--) {
+        if (a[i] == '1' && b[i] == '1' && carry == true) {
+            a[i] = '1';
+            carry = true;
+        } else if (a[i] == '1' && b[i] == '1' && carry == false) {
+            a[i] = '0';
+            carry = true;
+        } else if ((a[i] == '1' || b[i] == '1') && carry == true) {
+            // only a or b should have 1 if both have it above if should have taken it
+            a[i] = '0';
+            carry = true;
+        } else if (a[i] == '0' && b[i] == '0' && carry == true) {
+            a[i] = '1';
+            carry = false;
+        } else if ((a[i] == '1' || b[i] == '1') && carry == false) {
+            a[i] = '1';
+            carry = false;
+        } else if (a[i] == '0' && b[i] == '0' && carry == false) {
+            a[i] = '0';
+            carry = false;
+        }
+    }
+    return a;
 }
 
 int main(int ac, char *argv[])
@@ -97,7 +115,18 @@ int main(int ac, char *argv[])
         return -1;
     }
 
+    char *result = NULL;
+
     uint8_t op = get_op(argv[2][0]);
+    switch (op) {
+        case ADD:
+            result = get_sum(argv[1], argv[3]);
+            break;
+        default:
+            break;
+    }
+
+    printf("%s (%d)\n", result, to_int(result, 6));
 
     return 0;
 }

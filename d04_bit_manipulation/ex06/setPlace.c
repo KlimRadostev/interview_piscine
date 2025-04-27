d2ca1a415c25cddda9fdfcbd32d98fb2d8b604ab
Klim Radostev <klimradostev@Klims-MacBook-Pro.local>
2025-04-27 07:46:27 -0700

    42-INTERVIEW : ex06 completed

---
+++ d04_bit_manipulation/ex06/setPlace.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define BIT(x) (1U << (x))
+#define UNSET(x, y) ((x) & ~(BIT(y)))
+#define SET(x, y) ((x) | BIT(y))
+#define TOGGLE(x, y) ((x) ^ BIT(y))
+
+uint32_t set_place(uint32_t parking_row, int pos)
+{
+    return SET(parking_row, pos);
+}
+
+int main(int ac, char *argv[])
+{
+    if (ac != 3) {
+        return -1;
+    }
+
+    uint32_t new_prow = set_place(atoi(argv[1]), atoi(argv[2]));
+
+    printf("New parking row: %d\n", new_prow);
+
+    return 0;
+}
